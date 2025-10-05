1131a122b3582d80b013b8c1d55944d2676c64f2
Timothy Copeland <timothy.l.copeland2004@gmail.com>
2025-10-05 07:20:07 -0400

    M2HW_Copeland.cpp

--- M2HWQ1_Copeland.cpp
+++ M2HWQ1_Copeland.cpp
@@ -44,3 +44,19 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string school = "FTCC";
+    string team = "Trojans";
+
+    cout << "Let's go " << school << endl;
+    cout << "Let's go " << school << endl;
+    cout << "Let's go " << school << endl;
+    cout << "Let's go " << team << endl;
+
+    return 0;
+}
